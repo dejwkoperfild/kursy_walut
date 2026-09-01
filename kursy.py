e64d7ee7b5741f0a7112e9c89052edcbc4ddec52
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-01 18:33:53 +0200

    Zapis do pliku csv

--- kursy.py
+++ kursy.py
@@ -1,5 +1,6 @@
 import requests
 from requests.exceptions import RequestException
+import csv
 
 startDate = '2026-08-01'
 endDate = '2026-08-31'
@@ -11,10 +12,14 @@ try:
     response = requests.get(url, params = payload, timeout = 15)
     response.raise_for_status()
     data = response.json()
-    for kurs in data['rates']:
-        data_publikacji = kurs['effectiveDate']
-        wartosc_srednia = kurs['mid']
-        print(f"Data publikacji : {data_publikacji} | wartość średnia: {wartosc_srednia}")
+    with open('kursy.csv','w',newline='') as csvfile:
+        fieldnames = ['Date','rate']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for kurs in data['rates']:
+            data_publikacji = kurs['effectiveDate']
+            wartosc_srednia = kurs['mid']
+            writer.writerow({'Date': data_publikacji, 'rate': wartosc_srednia})
 
 
 except RequestException as e:
