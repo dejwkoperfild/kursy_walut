005e048266754eb92b4faa9e1efcb1f6705efaae
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-01 16:35:17 +0200

    Dodanie odpytania dla wskazanego zakresu dat

--- kursy.py
+++ kursy.py
@@ -1,15 +1,21 @@
 import requests
 from requests.exceptions import RequestException
 
+startDate = '2026-08-01'
+endDate = '2026-08-31'
+
 payload = {'format':'json'}
-url = "https://api.nbp.pl/api/exchangerates/rates/a/chf/"
+currency = 'chf'
+url = f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{startDate}/{endDate}"
 try:
     response = requests.get(url, params = payload, timeout = 15)
     response.raise_for_status()
     data = response.json()
-    rate = data['rates'][0]['mid']
-    date = data['rates'][0]['effectiveDate']
-    print(f"Aktualny kurs CHF w dniu {date} wynosi {rate} PLN")
+    for kurs in data['rates']:
+        data_publikacji = kurs['effectiveDate']
+        wartosc_srednia = kurs['mid']
+        print(f"Data publikacji : {data_publikacji} | wartość średnia: {wartosc_srednia}")
+
 
 except RequestException as e:
     print(f"Blad podczas komunikacji z API {e}")
