028ecb130fb1e08e98de6ba015400f8ef087187a
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-04 22:39:26 +0200

    Dodanie wykresu

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import csv
 from src.date_picker import get_dates
 from src.nbp_api import get_exchange_rates
+import matplotlib.pyplot as plt
 
 
 
@@ -18,6 +19,16 @@ if startDate and endDate and currency:
                 wartosc_srednia = kurs['mid']
                 writer.writerow({'Date': data_publikacji, 'rate': wartosc_srednia})
             print("Pomyślnie zapisano plik")
+
+        x_axis = [kurs['effectiveDate'] for kurs in data['rates']]
+        y_axis = [kurs['mid'] for kurs in data['rates']]
+        plt.plot(x_axis, y_axis)
+        plt.title(f"Wykres kursu {currency} od {startDate} do {endDate}")
+        plt.xlabel("Data")
+        plt.ylabel("Kurs")
+        plt.xticks(rotation=45)
+        path = f'output_files/{currency}_{startDate}-{endDate}.png'
+        plt.savefig(path, dpi=300, bbox_inches='tight')
 
     else:
         print("Nie udało się pobrać danych")
