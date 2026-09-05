7c77dcf971a4a62a7d3fae0e9200eade0b48a923
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-05 22:11:56 +0200

    Dodanie etykiet dla walut

--- app/main.py
+++ app/main.py
@@ -4,9 +4,10 @@ from src.view_data import get_data_from_user
 from src.nbp_api import get_exchange_rates
 from src.file_handler import save_to_csv, save_to_png
 
-currencies = ["chf", "eur", "usd", "sek", "gpb"]
+currencies = {"chf": "Frank szwajcarski", "eur": "Euro", "usd": "Dolar amerykański", "sek": "Korona szwedzka", "gpb": "Funt szterling"}
 days = 183
-startDate, endDate, currency = get_data_from_user(days, currencies)
+startDate, endDate, selected_label = get_data_from_user(days, currencies)
+currency = [k for k, v in currencies.items() if v == selected_label][0] if selected_label else None
 
 if startDate and endDate and currency:
     data = get_exchange_rates(currency, startDate, endDate)
