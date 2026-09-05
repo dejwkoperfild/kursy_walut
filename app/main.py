3cb65840e91a2bb299309d9c903685b172219665
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-05 21:28:03 +0200

    Wydzielenie funkcji do obsługi pliku

--- app/main.py
+++ app/main.py
@@ -2,9 +2,7 @@ import csv
 import datetime
 from src.view_data import get_data_from_user
 from src.nbp_api import get_exchange_rates
-import matplotlib.dates as mdates
-from datetime import datetime
-import matplotlib.pyplot as plt
+from src.file_handler import save_to_csv, save_to_png
 
 currencies = ["chf", "eur", "usd", "sek", "gpb"]
 days = 183
@@ -13,27 +11,8 @@ startDate, endDate, currency = get_data_from_user(days, currencies)
 if startDate and endDate and currency:
     data = get_exchange_rates(currency, startDate, endDate)
     if data:
-        with open(f'output_files/kursy_{currency}_{startDate}-{endDate}.csv','w',newline='') as csvfile:
-            fieldnames = ['Date','rate']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for kurs in data['rates']:
-                data_publikacji = kurs['effectiveDate']
-                wartosc_srednia = kurs['mid']
-                writer.writerow({'Date': data_publikacji, 'rate': wartosc_srednia})
-            print("Pomyślnie zapisano plik")
-
-        x_axis = [datetime.strptime(kurs['effectiveDate'], '%Y-%m-%d') for kurs in data['rates']]
-        y_axis = [kurs['mid'] for kurs in data['rates']]
-        plt.plot(x_axis, y_axis)
-        plt.title(f"Wykres kursu {currency} od {startDate} do {endDate}")
-        plt.xlabel("Data")
-        plt.ylabel("Kurs")
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
-        plt.gcf().autofmt_xdate()
-        path = f'output_files/{currency}_{startDate}-{endDate}.png'
-        plt.savefig(path, dpi=300, bbox_inches='tight')
+        save_to_csv(data, currency, startDate, endDate)
+        save_to_png(data, currency, startDate, endDate)
 
     else:
         print("Nie udało się pobrać danych")
