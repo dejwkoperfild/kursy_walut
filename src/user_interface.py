a3d01e312ebf1c9651020f6176bfb35da62bf109
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-08 18:51:33 +0200

    Dalsze prace nad nowym okienkiem

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,5 +1,3 @@
-from cProfile import label
-from logging import root
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry
@@ -11,7 +9,7 @@ class DateSelectorDialog:
     def __init__(self, min_date, max_date, currencies):
         self.root = tk.Tk()
         self.root.title("Kursy walut NBP")
-        self.root.geometry("350x200")
+        self.root.geometry("420x280")
 
         # create frames for the main view and the new tab
         self.main_frame = tk.Frame(self.root)
@@ -20,6 +18,8 @@ class DateSelectorDialog:
         # place the frames in the same location, but only one will be visible at a time
         self.main_frame.grid(row=0, column=0, sticky="nsew")
         self.new_frame.grid(row=0, column=0, sticky="nsew")
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
 
         self.main_frame.tkraise()
 
@@ -64,8 +64,24 @@ class DateSelectorDialog:
         self.combo.current(0)
         self.combo.grid(row=2, column=1, padx=10, pady=15)
 
-        tk.Button(self.main_frame, text="Zapisz i zamknij", command=self.save_and_close).grid(row=3, column=1, columnspan=2, pady=15)
+        tk.Button(self.main_frame, text="Zapisz i zamknij", command=self.save_and_close).grid(row=3, column=0, columnspan=2, pady=15, sticky="w")
 
+
+        tk.Label(self.new_frame, text="Kwota w PLN:").grid(row=0, column=0, padx=10, pady=(25, 10), sticky="w")
+        
+        tk.Label(self.new_frame, text="Waluta docelowa:").grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        
+        tk.Button(self.new_frame, text="Przelicz", command=self.convert_amount).grid(row=2, column=0, padx=10, pady=15)
+        tk.Button(self.new_frame, text="Wyczyść", command=self.clear_conversion).grid(row=2, column=1, padx=10, pady=15, sticky="e")
+        
+
+
+    def convert_amount(self):
+            pass
+
+    def clear_conversion(self):
+            pass
+    
 
     def save_and_close(self):
         self.results["start"] = self.calendar_from.get_date()
