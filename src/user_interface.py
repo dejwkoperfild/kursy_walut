479f73d6864b509513ed7041fe9b1e3017889861
dejwkoperfild <d.radziwoniuk@gmail.com>
2026-09-08 17:31:29 +0200

    Dodanie menu w GUI

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,12 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import DateEntry
+from tkinter import Menu
+
 
 class DateSelectorDialog:
     def __init__(self, min_date, max_date, currencies):
         self.root = tk.Tk()
         self.root.title("Wybór daty")
         self.root.geometry("350x200")
+
+        menu_bar = Menu(self.root)
+        self.root.config(menu=menu_bar)
+                
+        def about():
+            print("About this application")
+
+        file_menu = Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Exit", command=self.root.quit)
+
+       
+        help_menu = Menu(menu_bar, tearoff=0)
+        menu_bar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command=about)
         
         self.results = {"start": None, "end": None, "currency": None}
 
@@ -17,13 +34,13 @@ class DateSelectorDialog:
             self.root, width=15, mindate=min_date, maxdate=max_date, date_pattern='yyyy-mm-dd'
         )
 
-        tk.Label(self.root, text="Data początkowa (od):").grid(row=0, column=0, padx=10, pady=20, sticky="e")
+        tk.Label(self.root, text="Data początkowa (od):").grid(row=0, column=0, padx=10, pady=20, sticky="w")
         self.calendar_from.grid(row=0, column=1, padx=10, pady=20)
 
-        tk.Label(self.root, text="Data końcowa (do):").grid(row=1, column=0, padx=10, pady=5, sticky="e")
+        tk.Label(self.root, text="Data końcowa (do):").grid(row=1, column=0, padx=10, pady=5, sticky="w")
         self.calendar_to.grid(row=1, column=1, padx=10, pady=5)
 
-        tk.Button(self.root, text="Zapisz i zamknij", command=self.save_and_close).grid(row=2, column=1, columnspan=2, pady=15)
+        tk.Button(self.root, text="Zapisz i zamknij", command=self.save_and_close).grid(row=2, column=1, columnspan=2, pady=20)
 
         self.combo = ttk.Combobox(self.root, values=currencies, state="readonly")
         self.combo.current(0)
